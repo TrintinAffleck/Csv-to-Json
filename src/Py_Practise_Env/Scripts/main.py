82fd600787298ff19d3cd2ff63c3aef57fe36863
Trintin Affleck <t.affleck101@hotmail.com>
2022-08-30 15:47:54 -0600

    Refactored, error_check function created.

--- src/Py_Practise_Env/Scripts/main.py
+++ src/Py_Practise_Env/Scripts/main.py
@@ -1,6 +1,7 @@
 #Built-Ins
 '''Some of these are in other files'''
 from json import dumps
+from pydoc import doc
 
 #Locals
 from data import Data
@@ -16,6 +17,7 @@ test_id_marks_dict = {}
 marks = []        
 tests_taken = []        
 seen_student = set()
+
 def main():
 
     def get_avg(numbers: list):
@@ -24,76 +26,82 @@ def main():
         return 0
 
     def create_dicts(student_index:int): 
-        
-        student_df = Data.marks_df.groupby('student_id').get_group(student_index+1)
-
+        student_df = Data.marks_df.groupby('student_id').get_group(student_index)
         for row in student_df.itertuples(index=False):
             test_id,curr_student,mark = row
             if test_id not in tests_taken:
                 tests_taken.append(test_id)
-            #Clear our lists because we dont want a previous students tests to overlap with the current students tests.
-            #Didn't use .clear() because it overlaps with the next student if there is one.
-            #Create Marks list to keep track of student marks.
             marks.append(mark)
             if len(marks) > 0:
                 average_mark = get_avg(marks)
                 student_avg_mark_dict.update({f"{curr_student}": round(average_mark,2)})
                 if test_id not in test_id_marks_dict.keys():
                     test_id_marks_dict.update({test_id: mark})
-            #We need a list of test_ids for the dictionary.
         if student_index not in seen_student:
             seen_student.add(student_index)
         return student_test_id_dict
     
-    def update_student_keys(courses_taken:list, courses:list, index:int,averages):
-        student = Json.loaded_student[index]
-        total_avg = Data.marks_df.groupby('student_id').mark.mean().loc[student.get('id')]
+    def update_student_keys(courses_taken:list, 
+                            courses:list, 
+                            index:int,
+                            averages
+                            ):
+
+        student = Json.loaded_student[index-1]
+        df = Data.marks_df
+        total_avg = df.groupby('student_id').mark.mean().loc[student.get('id')]
         for course in Json.loaded_courses:
             if course.get('id') in courses_taken:
                 courses.append(course)
+                course.update({'courseAverage' : averages[course['id']]})
                 student.update({'totalAverage' : round(total_avg,2),
                                 'courses' : courses})
-                course.update({'courseAverage' : averages[course['id']]})
         return courses
 
     def get_courses_and_tests():
         '''Returns true if weights are not 100.'''
         student_index = 0
-        create_dicts(student_index)
         total_weight = 0
         courses_taken = []
         courses_avg_dict = {}
         course_avg = 0.0
         courses = []
         marks_list = []
-
-        for row in Data.tests_df.itertuples(index=False):
-            test_id,course_id,weight = row
-            if test_id in tests_taken:
-                if course_id not in courses_taken:
-                    courses_avg_dict[course_id] = 0
-                    courses_taken.append(course_id)
-                    marks_list = []
-                    #Weight cant be lower than 100 percent. So we throw an error key.
-                    if total_weight < 100 and total_weight != 0:
-                        print("Weight error: Course weights are under 100.")
+        def error_check(weight,marks_list:list):
+            total_weight = weight
+            for row in Data.tests_df.itertuples(index=False):
+                test_id,course_id,weight = row
+                if test_id in tests_taken:
+                    #TODO Problem is if student skips the a test and the next one taken is in not a new course the total weight is not reset
+                    if course_id not in courses_taken:
+                        courses_taken.append(course_id)
+                        marks_list = []
+                        #Weight cant be lower than 100 percent. So we throw an error key.
+                        if total_weight < 100 and total_weight != 0:
+                            print("Weight error: Course weights are under 100.")
+                            return True
+                        total_weight = 0
+                    marks_list.append(test_id_marks_dict.get(test_id))
+                    course_avg = get_avg(marks_list)
+                    courses_avg_dict.update({course_id : course_avg})
+                    total_weight += weight
+                    #Weight cant be higher than 100 percent. So we throw an error key.
+                    if total_weight > 100:
+                        print("Weight error: Course weights are over 100.")
                         return True
-                    total_weight = 0
-                marks_list.append(test_id_marks_dict.get(test_id))
-                course_avg = get_avg(marks_list)
-                courses_avg_dict.update({course_id : course_avg})
-                update_student_keys(courses_taken,courses,student_index,courses_avg_dict)
-                total_weight += weight
-                #Weight cant be higher than 100 percent. So we throw an error key.
-                if total_weight > 100:
-                    print("Weight error: Course weights are over 100.")
-                    return True
-        #So we can look at the data for the next student
-        student_index+=1
-        #Clearing variables for the new student
-        test_id_marks_dict.clear()
-        tests_taken.clear()
-        create_dicts(student_index)
+                else:
+                    if course_id not in courses_taken:
+                        total_weight = 0
+
+        for student in range(len(Data.students_df.groupby('id'))):
+            student_index+=1
+            create_dicts(student_index)
+            error_check(total_weight,marks_list)
+            update_student_keys(courses_taken,courses,student_index,courses_avg_dict)
+            #Clearing variables for the new student
+            test_id_marks_dict.clear()
+            tests_taken.clear()
+        get_courses_and_tests()
         return False
 
     if get_courses_and_tests() == True:
