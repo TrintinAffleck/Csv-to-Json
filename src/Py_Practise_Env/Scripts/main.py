87d0e21c69e9591107f238ae374346c094eafca6
Trintin Affleck <t.affleck101@hotmail.com>
2022-09-05 13:30:39 -0600

    split function into two functions

--- src/Py_Practise_Env/Scripts/main.py
+++ src/Py_Practise_Env/Scripts/main.py
@@ -1,31 +1,36 @@
 #Built-Ins
 '''Some of these are in other files'''
 from json import dumps
-from pydoc import doc
 
 #Locals
 from data import Data
 from convert import Json
 
 #3rd Party
-import pandas as pd
 
 #Global variables
 student_test_id_dict = {}
 student_avg_mark_dict = {}
-test_id_marks_dict = {}
-marks = []        
-tests_taken = []        
+
 seen_student = set()
+test_id_marks_dict = {}
+tests_taken = []        
+marks = []        
+marks_list = []
+courses_taken = []
+courses_avg_dict = {}
+courses = []
 
 def main():
-
+    student_index = 0
     def get_avg(numbers: list):
         if len(numbers) > 0:
             return round((sum(numbers) / len(numbers)),2)
         return 0
 
-    def create_dicts(student_index:int): 
+    def create_dicts(student_index:int):
+        '''Creates tests_taken, avg mark, student_avg_mark_dict, test_id_marks_dict,
+         and returns student_test_id_dict'''
         student_df = Data.marks_df.groupby('student_id').get_group(student_index)
         for row in student_df.itertuples(index=False):
             test_id,curr_student,mark = row
@@ -41,6 +46,25 @@ def main():
             seen_student.add(student_index)
         return student_test_id_dict
     
+    
+    def error_check(courses_taken:list,tests_taken:list,total_weight:float or int = 0):
+        seen_course = []
+        for row in Data.tests_df.itertuples(index=False):
+            test_id,course_id,weight = row
+            if test_id in tests_taken:
+                if course_id in courses_taken and course_id not in seen_course:
+                    seen_course.append(course_id)
+                    #Weight cant be lower than 100 percent. Return True.
+                    if total_weight < 100 and total_weight != 0:
+                        print("Weight error: Course weights are under 100.")
+                        return True
+                    total_weight = 0
+                total_weight += weight
+                #Weight cant be higher than 100 percent. Return True.
+                if total_weight > 100:
+                    print("Weight error: Course weights are over 100.")
+                    return True
+
     def update_student_keys(courses_taken:list, 
                             courses:list, 
                             index:int,
@@ -53,50 +77,34 @@ def main():
             if course.get('id') in courses_taken:
                 courses.append(course)
                 course.update({'courseAverage' : averages[course['id']]})
-                student.update({'totalAverage' : round(total_avg,2),
-                                'courses' : courses})
-        courses = courses
+                student.update({'totalAverage' : round(total_avg,2),'courses' : courses})
         return courses
-
-
-    '''Returns true if weights are not 100.'''
-    student_index = 0
-    total_weight = 0
-    courses_taken = []
-    courses_avg_dict = {}
-    course_avg = 0.0
-    courses = []
-    marks_list = []
-    for row in Data.tests_df.itertuples(index=False):
-        test_id,course_id,weight = row
-        if test_id in tests_taken:
-            #TODO Problem is if student skips the a test and the next one taken is in not a new course the total weight is not reset
-            if course_id not in courses_taken:
-                courses_taken.append(course_id)
-                marks_list = []
-                #Weight cant be lower than 100 percent. So we throw an error key.
-                if total_weight < 100 and total_weight != 0:
-                    print("Weight error: Course weights are under 100.")
-                    return True
-                total_weight = 0
-            total_weight += weight
-            #Weight cant be higher than 100 percent. So we throw an error key.
-            if total_weight > 100:
-                print("Weight error: Course weights are over 100.")
-                return True
-            marks_list.append(test_id_marks_dict.get(test_id))
-            course_avg = get_avg(marks_list)
-            courses_avg_dict.update({course_id : course_avg})
+        
+    def get_courses():
+        for row in Data.tests_df.itertuples(index=False):
+            test_id,course_id,weight = row
+            if test_id in tests_taken:
+                if course_id not in courses_taken:
+                    courses_taken.append(course_id)
+                    marks_list = []
+                marks_list.append(test_id_marks_dict.get(test_id))
+                course_avg = get_avg(marks_list)
+                courses_avg_dict.update({course_id : course_avg})
 
     for student in range(len(Data.students_df.groupby('id'))):
         student_index+=1
         create_dicts(student_index)
+        get_courses()
+        if error_check(courses_taken,tests_taken) == True:
+            break
         update_student_keys(courses_taken,courses,student_index,courses_avg_dict)
-        #Clearing variables for the new student
+        #Clearing variables for the next student
+        courses.clear()
+        courses_avg_dict.clear()
+        courses_taken.clear()
         test_id_marks_dict.clear()
         tests_taken.clear()
     
-
 if __name__ == '__main__':
     if  main() == True:
         report = {'error' : 'Invalid course weights'}
